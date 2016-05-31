1698bf28799102a0affae57d2dbc2b0c1efb2ef3
Nathan Fang <nyyrikkifang@gmail.com>
2016-05-31 17:25:03 +1000

    fix: Intl polyfill for Safari

--- app/bootstrapper.ts
+++ app/bootstrapper.ts
@@ -3,9 +3,12 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { HTTP_PROVIDERS } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import {IN_MEMORY_TASK_SERVICE_PROVIDERS} from './shared/services';
+import {
+  IN_MEMORY_TASK_SERVICE_PROVIDERS,
+  LOCAL_STORAGE_TASK_SERVICE_PROVIDERS
+} from './shared/services';
 
 bootstrap(AppComponent, [
   HTTP_PROVIDERS,
-  IN_MEMORY_TASK_SERVICE_PROVIDERS
+  LOCAL_STORAGE_TASK_SERVICE_PROVIDERS
 ]);
--- app/polyfills.ts
+++ app/polyfills.ts
@@ -1,1 +1,6 @@
 require('es6-shim');
+
+if (!global.Intl) {
+  global.Intl = require('intl');
+  require('intl/locale-data/jsonp/en.js');
+}
