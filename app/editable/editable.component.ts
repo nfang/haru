3f6baba1397de4c5efdf65e63b083e74a134cb39
Nathan Fang <nathan.fang@lmigroup.com>
2016-06-06 12:47:01 +1000

    refactor: adjust editable component interface

--- app/editable/editable.component.ts
+++ app/editable/editable.component.ts
@@ -1,12 +1,24 @@
 import {
+  forwardRef,
   Component,
   ViewChild,
-  Input,
   Renderer,
-  Output,
-  EventEmitter
+  Input,
+  AfterViewInit,
+  Provider
 } from '@angular/core';
+import {
+  NG_VALUE_ACCESSOR,
+  ControlValueAccessor
+} from '@angular/common';
 import { Observable } from 'rxjs/Observable';
+
+const noop = () => {};
+
+const EDITABLE_CONTROL_VALUE_ACCESSOR = new Provider(NG_VALUE_ACCESSOR, {
+  useExisting: forwardRef(() => EditableComponent),
+  multi: true
+});
 
 @Component({
   selector: 'editable',
@@ -14,26 +26,44 @@ import { Observable } from 'rxjs/Observable';
     require('./editable.component.scss')
   ],
   template: require('./editable.component.html'),
+  providers: [EDITABLE_CONTROL_VALUE_ACCESSOR],
   host: {
     '[class.editing]': '_isEditing'
   }
 })
-export class EditableComponent {
-  private _submitEmitter: EventEmitter<any> = new EventEmitter<any>();
-
-  @Output('submit')
-  get onSubmit(): Observable<any> {
-    return this._submitEmitter.asObservable();
-  }
-
-  @Input() model: string;
-  @Input() enabled: boolean;
+export class EditableComponent implements ControlValueAccessor, AfterViewInit {
+  private _onTouchedCallback: () => void = noop;
+  private _onChangeCallback: (v: any) => void = noop;
 
   private _isEditing: boolean = false;
+  private _value: string;
+
+  @Input() enabled: boolean;
+
+  get value(): string { return this._value; }
+
+  @Input() set value(arg: string) {
+    if (arg !== this._value) {
+      this._value = arg;
+      this._onChangeCallback(arg);
+    }
+  }
 
   @ViewChild('label') label;
 
-  constructor(private _renderer: Renderer) {}
+  constructor(private _renderer: Renderer) { }
+
+  writeValue(value: any) {
+    this._value = value;
+  }
+
+  registerOnChange(fn: any) {
+    this._onChangeCallback = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this._onTouchedCallback = fn;
+  }
 
   ngAfterViewInit() {
     this._renderer.listen(this.label.nativeElement, 'click', ((event) => {
@@ -50,7 +80,7 @@ export class EditableComponent {
 
   endEditing(event) {
     this._isEditing = false;
-    this._submitEmitter.emit(this.model);
     event.stopPropagation();
+    event.preventDefault();
   }
 }
